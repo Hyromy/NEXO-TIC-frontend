e5f8ac108f585c3e7ac2f5386d79be746bfda559
Hyromy <formulajoel9@gmail.com>
2026-02-23 12:01:22 -0600

    Add terms and conditions document in PDF format

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -39,3 +39,12 @@ export function ButtonGroup({
     </div>
   )
 }
+
+export function downloadFile(path: string, filename: string) {
+  const link = document.createElement('a')
+  link.href = "/" + path
+  link.download = filename
+  document.body.appendChild(link)
+  link.click()
+  document.body.removeChild(link)
+}
