9a873de17301d34c8ec69ef209dd182ecb9a3749
Hyromy <formulajoel9@gmail.com>
2026-02-26 19:39:56 -0600

    Add basic layout for home view with employee

--- src/layout/Containers.tsx
+++ src/layout/Containers.tsx
@@ -1,19 +1,99 @@
 import { type ReactNode } from "react"
 
+type BasicRange = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12
+type gap = 0 | 1 | 2 | 3 | 4 | 5
+
 type StackContainerProps = {
   children: ReactNode
   orientation?: "column" | "row"
-  gap?: 0 | 1 | 2 | 3 | 4 | 5
+  gap?: gap
   center?: boolean
+  height?: number
 }
 export function StackContainer({
   children,
   orientation = "column",
   gap = 2,
   center,
+  height,
 }: StackContainerProps) {
+  let classes = `d-flex flex-${orientation} gap-${gap}`
+  if (center) classes += " justify-content-center align-items-center"
+
   return (
-    <div className={`d-flex flex-${orientation} gap-${gap} ${center ? "justify-content-center align-items-center" : ""}`}>
+    <div className={classes} style={{height}}>
+      {children}
+    </div>
+  )
+}
+
+type RowContainerProps = {
+  children: ReactNode
+  gap?: gap
+}
+export function RowContainer({
+  children,
+  gap = 2,
+}: RowContainerProps) {
+  return (
+    <div className={`row gx-${gap} gy-${gap}`}>
+      {children}
+    </div>
+  )
+}
+
+type ColContainerProps = {
+  children: ReactNode
+  defaultSize?: BasicRange | true
+  xsm?: BasicRange
+  sm?: BasicRange
+  md?: BasicRange
+  lg?: BasicRange
+  xl?: BasicRange
+  xxl?: BasicRange
+  noPadding?: boolean
+}
+export function ColContainer({
+  children,
+  defaultSize = true,
+  xsm,
+  sm,
+  md,
+  lg,
+  xl,
+  xxl,
+  noPadding = false,
+}: ColContainerProps) {
+  let classes = noPadding ? "px-0" : ""
+  if (defaultSize == true) classes += " col"
+  else if (Number.isInteger(defaultSize)) classes += ` col-${defaultSize}`
+
+  if (xsm) classes += ` col-xsm-${xsm}`
+  if (sm) classes += ` col-sm-${sm}`
+  if (md) classes += ` col-md-${md}`
+  if (lg) classes += ` col-lg-${lg}`
+  if (xl) classes += ` col-xl-${xl}`
+  if (xxl) classes += ` col-xxl-${xxl}`
+
+  return (
+    <div className={classes}>
+      {children}
+    </div>
+  )
+}
+
+type ScrollableContainerProps = {
+  children: ReactNode
+  height?: string | number
+  forceHeight?: boolean
+}
+export function ScrollableContainer({
+  children,
+  height = 256,
+  forceHeight
+}: ScrollableContainerProps) {
+  return (
+    <div style={{ maxHeight: height, overflowY: "auto", height: forceHeight ? height : "auto" }}>
       {children}
     </div>
   )
--- src/pages/common/Dashboard.tsx
+++ src/pages/common/Dashboard.tsx
@@ -1,39 +1,213 @@
-import { useEffect } from "react"
-
-import { userService } from "../../services/nexotic"
-import useApi from "../../hooks/useApi"
-
 import Main from "../../layout/Main"
 
-import { Spinner } from "../../components/Spinner"
+import { Navigate } from "react-router-dom"
+import useUser from "../../hooks/useUser"
+import { Card } from "../../components/Card"
+import { List } from "../../components/List"
+import { Alert } from "../../components/Alert"
+import {
+  RowContainer,
+  ColContainer,
+  StackContainer,
+  ScrollableContainer
+} from "../../layout/Containers"
+import type { ReactNode } from "react"
+
+import { type icons, type variants } from "../../components/variants"
+
+const defaultHeight = 192
+const defaultPadding = 2
 
 export default function Dashboard() {
-  const { execute, data, error, loading } = useApi<any>()
+  const { canAccessEmployee, canAccessRRHH } = useUser()
 
-  useEffect(() => {
-    execute(userService.get())
-  }, [])
-
-  useEffect(() => {
-    if (error) {
-      console.error("Error fetching user data:", error)
-      alert("Error consiguiendo información de usuarios")
-    }
-  }, [error])
-
-  const content = loading
-  ? <Spinner />
-  : (
-    <>
-      <h1>Dashboard</h1>
-      <p>The content below is for debugging purposes only:</p>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </>
-  )
+  const main = () => {
+    if (canAccessEmployee()) return <EmployeeDashboard />
+    if (canAccessRRHH()) return <RRHHDashboard />
+    return <Navigate to="/" />
+  }
 
   return (
     <Main>
-      {content}
+      {main()}
     </Main>
+  )
+}
+
+type HolidaysCardProps = {
+  days: number
+}
+function DaysForHolidays({days}: HolidaysCardProps) {
+  return (
+    <Card header="Dias de vacaciones disponibles" padding={defaultPadding}>
+      <StackContainer center height={defaultHeight}>
+        <h2>{days} días</h2>
+      </StackContainer>
+    </Card>
+  )
+}
+
+type ItemsCardViewProps = {
+  items: string[]
+  height?: number
+}
+function PendingRequests({
+  items,
+  height = defaultHeight,
+}: ItemsCardViewProps) {
+  const content = items.length > 0
+    ? <List items={items} flush />
+    : <StackContainer center height={defaultHeight}>
+        No hay solicitudes pendientes
+      </StackContainer>
+
+  return (
+    <Card header="Solicitudes pendientes" padding={defaultPadding}>
+      <ScrollableContainer height={height} forceHeight>
+        {content}
+      </ScrollableContainer>
+    </Card>
+  )
+}
+
+type AlertObject = {
+  icon?: icons
+  children: ReactNode
+  variant?: variants
+}
+type NoticesProps = {
+  items: AlertObject[]
+  height?: number
+}
+function Notices({
+  items,
+  height = defaultHeight * 2 + 74,
+}: NoticesProps) {
+  const content = items.length > 0
+    ? items.map((item, index) => (
+      <Alert key={index} notDismissible icon={item.icon} type={item.variant}>
+        {item.children}
+      </Alert>)
+    )
+    : <StackContainer center height={defaultHeight}>
+        No hay avisos o informes
+      </StackContainer>
+
+  return (
+    <Card header="Avisos e informes" padding={defaultPadding}>
+      <ScrollableContainer height={height} forceHeight>
+        {content}
+      </ScrollableContainer>
+    </Card>
+  )
+}
+
+type IncidentsProps = {
+  items: IndicentObject[]
+  height?: number
+}
+function Incidents({
+  items,
+  height = defaultHeight,
+}: IncidentsProps) {
+  return (
+    <Card header="Incidencias pendientes" padding={defaultPadding}>
+      <ScrollableContainer height={height} forceHeight>
+        {items.length > 0
+          ? <List
+              items={items.map(item => (
+                <StackContainer >
+                  {item.title} - {item.date}
+                </StackContainer>
+              ))}
+              flush
+            />
+          : <StackContainer center height={height}>
+              No hay incidencias pendientes
+            </StackContainer>
+        }
+      </ScrollableContainer>
+    </Card>
+  )
+}
+
+type IndicentObject = {
+  title: string
+  date: string
+}
+function EmployeeDashboard() {
+  const pendingRequests = [
+    "Solicitud de permiso 1",
+    "Solicitud de permiso 2",
+  ]
+  const notices: AlertObject[] = [
+    { icon: "info", children: "Información importante", variant: "info" },
+    { icon: "warning", children: "Advertencia", variant: "warning" },
+    { icon: "warning", children: "Advertencia", variant: "warning" },
+  ]
+  const incidents: IndicentObject[] = [
+    { title: "Incidente 1", date: "2023-01-01" },
+    { title: "Incidente 2", date: "2023-01-02" },
+  ]
+
+  return (
+    <__EmployeeLayout
+      first={<DaysForHolidays days={4} />}
+      second={<PendingRequests items={pendingRequests} />}
+      tall={<Notices items={notices} />}
+      fat={<Incidents items={incidents} />}
+    />
+  )
+}
+
+type EmployeeLayoutProps = {
+  first: ReactNode
+  second: ReactNode
+  tall: ReactNode
+  fat: ReactNode
+}
+function __EmployeeLayout({
+  first,
+  second,
+  tall,
+  fat,
+}: EmployeeLayoutProps) {
+  const gap = 3
+
+  return (
+    <RowContainer gap={gap}>
+      <ColContainer defaultSize={8} sm={12} lg={8}>
+        <StackContainer gap={gap}>
+          <RowContainer gap={gap}>
+            <ColContainer sm={12} lg={6}>
+              {first}
+            </ColContainer>
+            <ColContainer sm={12} lg={6}>
+              {second}
+            </ColContainer>
+          </RowContainer>
+          <RowContainer gap={gap}>
+            <ColContainer defaultSize={12}>
+              {fat}
+            </ColContainer>
+          </RowContainer>
+        </StackContainer>
+      </ColContainer>
+      <ColContainer defaultSize={4} sm={12} lg={4}>
+        {tall}
+      </ColContainer>
+    </RowContainer>
+  )
+}
+
+function RRHHDashboard() {
+  return (
+    <__RRHHLayout />
+  )
+}
+
+function __RRHHLayout() {
+  return (
+    <h1>RRHH Layout</h1>
   )
 }
